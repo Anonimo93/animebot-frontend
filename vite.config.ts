d2c4b694433df318d04c6b6f8ddbf52e1dbab45b
root <root@vmi2173083.contaboserver.net>
2026-06-09 12:54:41 +0200

    Fix base path for animebot-frontend repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/animebot-webapp/",
+  base: "/animebot-frontend/",
   build: {
     outDir: "dist",
   },
